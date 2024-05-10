f058cd653f4783b39a38d5f2185445d1fae5c495
Ojee Afolabi <sylvesterojay@gmail.com>
2024-05-10 15:31:49 +0100

    modal for mobile and desktop

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,35 +1,76 @@
 // import React from 'react'
+import { useState } from "react";
+import NavModal from "./UI/NavModal";
 
 const Header = () => {
+  const [src, setSrc] = useState("icons/hamburger.svg");
+  const [modal, setModal] = useState(false);
+
+  const clickHandler = () => {
+    if (src === "icons/hamburger.svg") {
+      setSrc("icons/clear.svg");
+      setModal(true)
+    } else {
+      setSrc("icons/hamburger.svg");
+      setModal(false)
+    }
+  };
+
   return (
-    <div className="bg-slate-500 flex justify-between items-center p-[10px]">
-      <div>
-        <img src="" alt="" />
-        <img className="w-[80px] m-0 h-[40px]" src="icons/amazon.svg" alt="" />
-      </div>
-      <div className="md:flex hidden">
-        <input
-          className=" object-contain text-center rounded xl:w-[50rem]  w-[30rem]"
-          type="text"
-        />
-        <img
-          className="h-[40px] bg-slate-700 rounded"
-          src="icons/search.svg"
-          alt=""
-        />
-      </div>
-      <div className="flex items-center gap-[4rem] ">
-        <div className="flex flex-col">
-          <span>Hello, Guest</span>
-          <span className="mt-0 mb-0">
-            <strong>Sign In</strong>
-          </span>
+    <div className="bg-slate-500 justify-center flex-col w-[100%] pb-1 pt-1">
+      <div className="w-[100%] border flex justify-between items-center ">
+        <div className="flex">
+          <img
+            onClick={clickHandler}
+            className="w-[40px] "
+            src={src}
+            alt=""
+          />
+          <img
+            className="w-[80px] m-0 h-[40px]"
+            src="icons/amazon.svg"
+            alt=""
+          />
         </div>
 
-        <div>
-          <img className="h-[40px] mr-4" src="icons/cart.svg" alt="" />
+        <div className="md:flex hidden">
+          <input className="focus:outline-none text-center rounded w-[30rem]" type="text" />
+          <img
+            className="w-[2em] bg-slate-700 rounded"
+            src="icons/search.svg"
+            alt=""
+          />
+        </div>
+
+        <div className="flex items-center gap-[2rem] border ">
+          <div className="flex flex-col">
+            <span className="md:ml-3 hidden md:flex">Hello, Guest</span>
+            <span className="md:hidden">Guest</span>
+            <span className="md:ml-3 mt-0 mb-0">
+              <strong>Sign In</strong>
+            </span>
+          </div>
+
+          <div>
+            <img className="w-[2em] mr-4" src="icons/cart.svg" alt="" />
+          </div>
         </div>
       </div>
+
+
+      {/* SEARCH INPUT FOR MOBILE VIEW */}
+      <div className="w-[100%] mt-2 flex justify-center items-center md:hidden py-1 px-2 ">
+        <div className="flex bg-white rounded w-[inherit]">
+        <input className="focus:outline-none text-center rounded w-[90%] p-2" type="text" />
+        <span className="w-[10%] px-2 py-2 bg-[orange] rounded "><img className="w-[2em]" src="icons/search.svg" alt="" /></span>
+        </div>
+      </div>
+
+      {
+      modal && 
+       <NavModal/>
+      }
+      
     </div>
   );
 };
---
+++ client/src/components/UI/NavModal.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const NavModal = () => {
+  const [close, setClose] = useState();
+
+  return (
+    <div className="flex fixed top-0 left-0 border border-green-700 h-[100vh] w-[100%] bg-[rgb(0,0,0,0.8)]">
+      <div className="w-[80%] md:w-[20%] flex-col border border-red-700">
+        <button className="bg-slate-900 w-[100%] text-white p-4 text-2xl">
+          Hello Guest
+        </button>
+        <div className="bg-[white] pl-8 flex flex-col gap-[1em]">
+          <p className="text-lg mt-4 font-bold">Digital Content and Devices</p>
+          <p>Amazon Music</p>
+          <p>Kindle E-readers & Books</p>
+          <p>Amazon Appstore</p>
+          <hr />
+            <p className="text-lg font-bold">Shop By Department</p>
+            <p>Electronics</p>
+            <p>Computers</p>
+            <p>Smart Home</p>
+            <p>Arts & Crafts</p>
+            <hr />
+          
+            
+            <p className="text-lg font-bold">Programs & Features</p>
+            <p>Gift Cards</p>
+            <p>Shop By Interest</p>
+            <p>Amazon Live</p>
+            <p>International Shipping</p>
+            <hr />
+          
+
+            <p className="text-lg font-bold">Help & Settings</p>
+            <p>Electronics</p>
+            <p>Computers</p>
+            <p>Smart Home</p>
+            <p>Arts & Crafts</p>
+            <hr />
+          
+        </div>
+      </div>
+
+      <div className="p-5 border border-[blue]">
+        <span>
+          <img className="w-[2em]" src="icons/clear.svg" alt="" />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default NavModal;
