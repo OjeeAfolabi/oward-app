bd000b2ce9c56ee2eedebe7cb3c6063c9ab0dd86
OjeeAfolabi <afolabiojee@gmail.com>
2025-09-15 16:46:33 +0100

    update client

--- client/src/Page/Home.jsx
+++ client/src/Page/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
 
   return (
     <div className="w-[100%] flex flex-col justify-center overflow-x-hidden">
-      <div className="relative w-full md:h-[100vh] mt-[6.5em] bg-[url('images/amazonbg.jpg')] bg-cover md:mt-[3em] ">
+      <div className="relative w-full md:h-[100vh] mt-[6.5em] bg-[url('/images/amazonbg.jpg')] bg-cover md:mt-[3em] ">
         
         <div className="pt-6 md:absolute top-0 md:top-[auto] md:bottom-0 left-0 w-[100%] justify-center items-center md:flex-row flex md:gap-[2rem] flex-col gap-3">
           {json.map((items, index)=> {
