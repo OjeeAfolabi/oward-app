3018a030807ffaf03c962b52e663875096811ee5
OjeeAfolabi <afolabiojee@gmail.com>
2025-09-20 14:02:16 +0100

    server update

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -74,7 +74,9 @@ const loginUser = async (req, res) => {
       httpOnly: true,
       secure: true,
       sameSite: "none",
+      domain: "https://oward-app.vercel.app",
       maxAge: maxAge * 1000,
+      path: "/",
     });
     res.status(201).json({ user });
   } catch (error) {
